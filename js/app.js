0dbd59f16f8d861603b426e42c7ede6bdc292559
ethgods <105787631+ethgods@users.noreply.github.com>
2022-05-18 15:46:29 -0600

    price

--- js/app.js
+++ js/app.js
@@ -127,7 +127,7 @@ async function checkChain() {
 
 async function loadInfo() {
   window.info = await contract.methods.currentTokenId().call();
-  window.mintPrice = 0.015;//await contract.methods.getPrice(1).call();
+  window.mintPrice = 0.005;//await contract.methods.getPrice(1).call();
   // console.log('currentTokenId: ' + window.info);
 
   let totalSupplyOnchain = await contract.methods.totalSupply().call();
@@ -285,7 +285,7 @@ async function mint() {
   mintButton.innerHTML = spinner;
 
   const amount = parseInt(document.getElementById("mintInput").value);
-  const value = 0.015 * amount;
+  const value = 0.005 * amount;
   const publicMintActive = true; //await contract.methods.mintingActive().call();
   const presaleMintActive = true; //await contract.methods.presaleActive().call();
 
